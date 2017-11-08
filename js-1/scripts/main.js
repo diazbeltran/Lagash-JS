c1f6a336152c41d1aa98cca275a5badef77b5a82
Marcelo <extremodiaz@gmail.com>
2017-11-08 12:29:52 -0300

    Cambios en el JS

--- js-1/scripts/main.js
+++ js-1/scripts/main.js
@@ -7,14 +7,14 @@ alert("Todos estan Muertos");
 
 
 
-var weapon = prompt("Mira, un almacen, al parecer solo hay un hacha y una pistola, rapido toma un objeto");
+var Arma = prompt("Mira, un almacen, al parecer solo hay un hacha y una pistola, rapido toma un objeto");
 
-var randomNumber = Math.round(Math.random());
+var numeroAlazar = Math.round(Math.random());
 
-alert("Un Zombie salvaje aparece, rapido utiliza tu " + " " + weapon);
+alert("Un Zombie salvaje aparece, rapido utiliza tu " + " " + Arma);
 
-if(randomNumber === 0 ){
-  alert("The zombie bites you.  You lose!!!");
-} else if (randomNumber === 1 ){
-  alert("You kill the zombie with your" + " " + weapon + ". You win!");
+if(numeroAlazar === 0 ){
+  alert("Tu "+ Arma + " fallo y el Zombie te hizo suyo. Perdiste!!!");
+} else if (numeroAlazar === 1 ){
+  alert("Perfecto que le haz dado un critico con tu " + " " + Arma + ". Sigues Vivo!!");
 }
